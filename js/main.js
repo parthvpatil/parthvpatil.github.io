5bba0a3af08fee88f1ef97177418c13ceb19de2b
parth <parthvin@gmail.com>
2019-05-21 14:40:50 +0530

    added new tutrorial menu

--- js/main.js
+++ js/main.js
@@ -17,7 +17,20 @@ $(document).ready(function () {
     }
   })
 
-  if (window.location.hash && window.location.hash == '#blog') {
+  $('a.tutorial-button').click(function (e) {
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
+  })
+
+
+  if (window.location.hash && window.location.hash == '#blog' && window.location.hash == '#tutorial') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
@@ -34,5 +47,11 @@ $(document).ready(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
+
+  $('.navigation-wrapper .tutorial-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+
 
 })
