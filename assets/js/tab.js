7371fb0a1f2567d26ff285c2407fbdf0c5471988
parth <parthvin@gmail.com>
2020-04-25 02:11:20 +0530

    added the implementation of changing tabs

---
+++ assets/js/tab.js
@@ -0,0 +1,17 @@
+function openTab(evt, sectionName, level) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    if(tabcontent[i].className.split(" ")[1] >= level){
+      tabcontent[i].style.display = "none";
+    }
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    if(tablinks[i].className.split(" ")[1] >= level){
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  }
+  document.getElementById(sectionName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
